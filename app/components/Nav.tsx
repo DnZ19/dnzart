e6acc183a68eb7d4fcc9665143f0ba8f7a50d646
DnZ19 <dennis@centerofagile.nl>
2023-05-07 11:26:37 +0200

    links in nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,22 +1,29 @@
 "use client";
+import Link from "next/link";
 
 export default function Nav() {
 	return (
 		<nav>
-			<ul className="flex items-centerpy-8">
-				<li className="flex flex-row justify-between items-center bg-teal-600 text-white py-2 px-4 rounded-md">
+			<ul className="flex items-centerpy-8 text-gray-800">
+				<Link href="/">
+					<li className="flex flex-row justify-between items-center py-2 px-4 rounded-md">
+						Home
+					</li>
+				</Link>
+
+				<Link href="/products">
+					<li className="flex flex-row justify-between items-center py-2 px-4 rounded-md">
+						overview
+					</li>
+				</Link>
+
+				<li className="flex flex-row justify-between items-center py-2 px-4 rounded-md">
+					about me
+				</li>
+				<li className="flex flex-row justify-between items-cente py-2 px-4 rounded-md">
 					link
 				</li>
-				<li className="flex flex-row justify-between items-center bg-teal-600 text-white py-2 px-4 rounded-md">
-					link
-				</li>
-				<li className="flex flex-row justify-between items-center bg-teal-600 text-white py-2 px-4 rounded-md">
-					link
-				</li>
-				<li className="flex flex-row justify-between items-center bg-teal-600 text-white py-2 px-4 rounded-md">
-					link
-				</li>
-				<li className="flex flex-row justify-between items-center bg-teal-600 text-white py-2 px-4 rounded-md">
+				<li className="flex flex-row justify-between items-center py-2 px-4 rounded-md">
 					link
 				</li>
 			</ul>
